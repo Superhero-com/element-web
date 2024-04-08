e2bd050b7066c51188abf9c6df921f79f3ca3198
Keno Dressel <hire@keno.digital>
2024-04-08 17:58:26 +0200

    fix: get room name

--- src/hooks/useRoomName.ts
+++ src/hooks/useRoomName.ts
@@ -1,34 +1,36 @@
-import { IPublicRoomsChunkRoom, Room } from "matrix-js-sdk/src/matrix";
-import { getDisplayAliasForAliasSet } from "matrix-react-sdk/src/Rooms";
+import { Room, RoomEvent } from "matrix-js-sdk/src/matrix";
 import { _t } from "matrix-react-sdk/src/languageHandler";
 import { IOOBData } from "matrix-react-sdk/src/stores/ThreepidInviteStore";
+import { useEffect, useState } from "react";
+import { useTypedEventEmitter } from "matrix-react-sdk/src/hooks/useEventEmitter";
 
-/**
- * Removes the [TG] prefix and leading whitespace from a room name
- * @param roomName
- * @returns {string}
- */
+export const getRoomName = (room?: Room, oobName = ""): string => getSafeRoomName(room?.name || oobName);
+
 export function getSafeRoomName(roomName?: string): string {
     return roomName?.replace(/^(\s|\[TG\])*/, "").replace(/^(\s|\$)*/, "") || "";
 }
-
 /**
  * Determines the room name from a combination of the room model and potential
+ * out-of-band information
  * @param room - The room model
  * @param oobData - out-of-band information about the room
  * @returns {string} the room name
  */
-export function getRoomName(room?: Room | IPublicRoomsChunkRoom, oobName?: IOOBData): string {
-    const roomName =
-        room?.name ||
-        oobName?.name ||
-        getDisplayAliasForAliasSet(
-            (room as IPublicRoomsChunkRoom)?.canonical_alias ?? "",
-            (room as IPublicRoomsChunkRoom)?.aliases ?? [],
-        ) ||
-        _t("common|unnamed_room");
+export function useRoomName(room?: Room, oobData?: IOOBData): string {
+    let oobName = _t("common|unnamed_room");
+    if (oobData && oobData.name) {
+        oobName = oobData.name;
+    }
 
-    return getSafeRoomName(
-        (roomName || "").replace(":", ":\u200b"), // add a zero-width space to allow linewrapping after the colon (matrix defaults)
-    );
+    const [name, setName] = useState<string>(getRoomName(room, oobName));
+
+    useTypedEventEmitter(room, RoomEvent.Name, () => {
+        setName(getRoomName(room, oobName));
+    });
+
+    useEffect(() => {
+        setName(getRoomName(room, oobName));
+    }, [room, oobName]);
+
+    return name;
 }
